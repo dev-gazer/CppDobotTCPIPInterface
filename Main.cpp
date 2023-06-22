ef00cf8c8ed2259a2e20055165e35c038ce9304c
Alexandre Martuscelli Faria <martuscellifaria@gmail.com>
2023-06-22 10:26:18 +0200

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include "DobotPorts/DobotDashboard.hpp"
+#include "DobotPorts/DobotMove.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[]) { 
+    DobotDashboard dobotDash("192.168.1.6", 29999);
+    DobotMove dobotMove("192.168.1.6", 30003);
+    std::cout << dobotDash.enableRobot() << std::endl;
+    std::cout << dobotMove.clearError() << std::endl;
+    std::cout << dobotDash.getPose() << std::endl;
+    std::cout << dobotDash.disableRobot() << std::endl;
+    return 0;
+}
