7b2428b5ddded2f7718ca987ca22e90d4cf9defc
Alexandre Martuscelli Faria <martuscellifaria@gmail.com>
2023-06-22 15:00:51 +0200

    Add files via upload

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,10 +6,11 @@
 int main(int argc, char* argv[]) { 
     DobotDashboard dobotDash("192.168.1.6", 29999);
     DobotMove dobotMove("192.168.1.6", 30003);
+    Utils utils;
     std::cout << dobotDash.enableRobot() << std::endl;
     std::cout << dobotMove.clearError() << std::endl;
     std::cout << dobotDash.getPose() << std::endl;
     std::cout << dobotDash.disableRobot() << std::endl;
-    parseToPose(dobotDash.getPose());
+    utils.parseToPose(dobotDash.getPose());
     return 0;
 }
