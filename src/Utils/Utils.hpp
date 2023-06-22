39ac3afa4c6a8f39d468b56286fc2f844bc694fb
Alexandre Martuscelli Faria <martuscellifaria@gmail.com>
2023-06-22 14:49:28 +0200

    Add files via upload

--- src/Utils/Utils.hpp
+++ src/Utils/Utils.hpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
 
+std::vector<std::string> splitDobotResponse(const std::string &s, char delim) {
+    std::vector<std::string> result;
+    std::stringstream ss (s);
+    std::string item;
+
+    while (getline (ss, item, delim)) {
+        result.push_back (item);
+    }
+
+    return result;
+}
+
+std::vector<float> parseToPose(const std::string &s) {
+    std::vector<std::string> v = splitDobotResponse(s, '{');
+    std::vector<std::string> posString = splitDobotResponse(v[1], '}');
+    std::vector<float> result;
+    std::stringstream ss (posString[0]);
+    std::string item;
+
+    while (getline (ss, item, ',')) {
+        result.push_back(::atof(item.c_str()));
+    }
+
+    return result;
+}
