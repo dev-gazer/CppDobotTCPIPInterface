f4a533dbd705791ff90508683ee8594cf869d832
Alexandre Martuscelli Faria <martuscellifaria@gmail.com>
2023-06-26 07:40:26 +0200

    Update DobotMove.cpp

--- src/DobotPorts/DobotMove.cpp
+++ src/DobotPorts/DobotMove.cpp
@@ -1,21 +1,21 @@
 #include "DobotMove.hpp"
 
-std::string DobotMove::movJ(float x, float y, float z, float rz){
+std::string DobotMove::movJ(const float &x, const float &y, const float &z, const float &rz){
     DobotMove::sendMsg("MovJ(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + "," + std::to_string(rz) + ")");
     return DobotMove::waitReply();
 }
 
-std::string DobotMove::movL(float x, float y, float z, float rz){
+std::string DobotMove::movL(const float &x, const float &y, const float &z, const float &rz){
     DobotMove::sendMsg("MovL(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + "," + std::to_string(rz) + ")");
     return DobotMove::waitReply();
 }
 
-std::string DobotMove::relMovL(float ox, float oy, float oz){
+std::string DobotMove::relMovL(const float &ox, const float &oy, const float &oz){
     DobotMove::sendMsg("RelMovL(" + std::to_string(ox) + "," + std::to_string(oy) + "," + std::to_string(oz) + ")");
     return DobotMove::waitReply();
 }
 
-std::string DobotMove::movJog(std::string axisId){
+std::string DobotMove::movJog(const std::string &axisId){
     DobotMove::sendMsg("MovJog(" + axisId + ")");
     return DobotMove::waitReply();
 }
